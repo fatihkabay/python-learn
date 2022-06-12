4deab02caae1a548b553dfd4f4d4997a4d20bdb3
Fatih Kabay <mfthkabay@gmail.com>
2022-06-12 11:46:50 +0300

    first project

---
+++ adamAsmaca.py
@@ -0,0 +1,87 @@
+import random
+
+resim = ["""
+   +---+
+   |   |
+       |
+       |
+       |
+       |
+--------""","""
+   +---+
+   |   |
+   O   |
+       |
+       |
+       |
+--------""","""
+   +---+
+   |   |
+   O   |
+   |   |
+       |
+       |
+--------""","""
+   +---+
+   |   |
+   O   |
+  /|   |
+       |
+       |
+--------""","""
+   +---+
+   |   |
+   O   |
+  /|  |
+       |
+       |
+--------""","""
+   +---+
+   |   |
+   O   |
+  /|  |
+  /    |
+       |
+--------""","""
+   +---+
+   |   |
+   O   |
+  /|  |
+  /   |
+       |
+--------"""]
+
+while True:
+    print(("----->") + "Adam Asmaca Oyunu" + ("<-----"))
+    
+    kelime = random.choice(["beþiktaþ", "tuzlaspor", "terminal", "trabzon", "galatasaray", "fenerbahçe"])
+    adim = 0
+    tahmin = []
+   
+    
+    while True:
+        print(resim[adim])
+        
+        for i, char in enumerate(kelime):
+            print(char if i in tahmin else "_"),
+            
+        cevap = input("Kelimeyi Tahmin Edin: ")
+        
+        if cevap == kelime:
+            print("Kazandýnýz!")
+            break
+        else:
+            while True:
+                rastgele = random.randint(0, len(kelime))
+                if not rastgele in tahmin:
+                    tahmin.append(rastgele)
+                    break
+            
+            adim += 1
+        
+        if adim >= len(resim):
+            print("Kaybettiniz!")
+            break
+        
+    if not "y" == input("Tekrar Oynamak Ýstermisiniz? (y/n): "):
+        break
