dbcaa02a8ffd30d08107ae2bcb32eedc3a89c744
Fatih Kabay <mfthkabay@gmail.com>
2022-06-13 21:37:55 +0300

    finish project

---
+++ negative-or-positive.py
@@ -0,0 +1,10 @@
+sayi = input('Bir Sayı Giriniz : ')
+ 
+if(int(sayi)>0):
+     print(sayi +" Sayısı Pozitif Sayıdır.")
+ 
+if(int(sayi)<0):
+     print(sayi +" Sayısı Negatif Sayıdır.")
+ 
+if(int(sayi)==0):
+     print(sayi +" Sayısı Sıfırdır.")
