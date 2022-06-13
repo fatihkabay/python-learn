696031e20079369c06de1b09b2ad240ce036135c
Fatih Kabay <mfthkabay@gmail.com>
2022-06-13 21:42:34 +0300

    finish project

---
+++ multiplesNumbers.py
@@ -0,0 +1,10 @@
+#hangi sayıların 5'in katı olduğunu söyleyen python kodu
+
+sayilar = [18,15,22,19,85,32,65,30,95,10,12,20,32,55,34,28,101,5,4,32]
+sayac=0 
+for sayi in sayilar:
+  if sayi%5 == 0:
+    print ( "{} sayısı 5'in katıdır.".format(sayi))
+    sayac=sayac+1
+ 
+print("5'in katı olan sayı adeti : "+str(sayac))
