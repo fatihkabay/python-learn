7da72f3874ab03bd56998e295b2934851b458c32
Fatih Kabay <mfthkabay@gmail.com>
2022-06-13 21:39:43 +0300

    finish project

---
+++ rectangularArea.py
@@ -0,0 +1,11 @@
+
+def dikdortgenAlan(genislik, yukseklik):
+  alan = genislik * yukseklik
+  print ("Alan :",alan)
+  return alan
+ 
+gen = float(input("Genişlik :"))
+ 
+yuk = float(input("Yükseklik : "))
+ 
+dikdortgenAlan(gen, yuk)
