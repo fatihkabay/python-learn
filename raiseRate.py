7d978f354458263f588f9729e41b1b1b206aae2c
Fatih Kabay <mfthkabay@gmail.com>
2022-06-14 21:28:18 +0300

    finish project

--- raiseRate.py
+++ raiseRate.py
@@ -1,5 +1,5 @@
 yeniMaas=0
 maas=int(input("Maaþý Gir : "))
-zam=int(input("Zam Oraný(%) : "))
+zam=int(input("Zam Oraný(%) : ")) #maaþýn girilen deðere göre zamlý maaþý bulma kodu
 yeniMaas=int(maas)+(int(maas)*int(zam)/100)
 print("Zamlý Maaþ :",yeniMaas)
