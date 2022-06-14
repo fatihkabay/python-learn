44194291f37beb08d30c7fd94fb4fa6a4c04fca2
Fatih Kabay <mfthkabay@gmail.com>
2022-06-14 21:31:23 +0300

    finish project

---
+++ average.py
@@ -0,0 +1,5 @@
+y1 = float(input('1. Yazýlý Notunu Giriniz : '))
+y2 = float(input('2. Yazýlý Notunu Giriniz: '))
+y3 = float(input('3. Yazýlý Notunu Giriniz: '))
+ortalama=(y1+y2+y3)/3
+print("Dersin Ortalamasý :{0} ".format(ortalama))
