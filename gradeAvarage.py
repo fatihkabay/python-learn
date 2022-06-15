6511a19a587cd3686739ac7cfdc7ae6bcda92e82
Fatih Kabay <mfthkabay@gmail.com>
2022-06-15 12:10:21 +0300

    finish project

---
+++ gradeAvarage.py
@@ -0,0 +1,10 @@
+#girilen ortalama deðerinin sonucunu gösteren kodlardýr. 
+ortalama=float(input("Not ortalamanýzý giriniz: "))
+if ortalama>=85:
+ print("Takdir Belgesi Alamaya Hak Kazandýnýz!")
+elif ortalama>=70:
+ print("Teþekkür Belgesi Alamaya Hak Kazandýnýz!")
+elif ortalama>=50:
+ print("Sýnýfý Geçtiniz")
+else:
+ print("Sýnýf tekrarý...")
