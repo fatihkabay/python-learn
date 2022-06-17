4627481740ebb5f9bfdb81be11cdf3b99a188c55
Fatih Kabay <mfthkabay@gmail.com>
2022-06-17 20:09:50 +0300

    finish project

---
+++ password.py
@@ -0,0 +1,8 @@
+parola= input("Parolayý giriniz:")
+if parola=="biliþim":
+ print ("Hoþgeldiniz...")
+ print ("Yönlendiriliyorsunuz...")
+else:
+ print ("Geçersiz parola")
+ print ("Tekrar dene")
+print ("---------------------------------")
