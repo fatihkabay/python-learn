5670fe19218b764788f168ad5dd3f04cce4b1850
Fatih Kabay <mfthkabay@gmail.com>
2022-06-17 20:17:34 +0300

    finish project

---
+++ randomNumber.py
@@ -0,0 +1,5 @@
+#liste içerisindeki herhangi bir sayıyı seçen yazılım dosyasıdır.
+import random
+liste=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+sayi=random.choice(liste)
+print (sayi)
