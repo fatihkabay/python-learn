71988f09c5de0ed8b1463503af6a3120da8eb2c6
Fatih Kabay <mfthkabay@gmail.com>
2022-06-17 20:14:21 +0300

    finish project

---
+++ divisibilitiy.py
@@ -0,0 +1,7 @@
+#bir sayýnýn sýfýra tam bölünüp bölünmediðini gösteren yazýlým dosyasýdýr.
+bolum=int(input("Bölünecek sayýyý giriniz: "))
+bolen=int(input("Bölen sayýyý giriniz "))
+if bolen!=0:
+ print("Sonuç: ", bolum/bolen)
+else:
+ print("Sýfýra bölme iþlemi yapýlamaz!")
