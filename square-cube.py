d6e306409e861e92167c303532e49e3df6c65b1d
Fatih Kabay <mfthkabay@gmail.com>
2022-06-20 23:10:39 +0300

    finish project

---
+++ square-cube.py
@@ -0,0 +1,4 @@
+#bir sayının hem karesini hemde küpünü veren kodlardır.
+sayi=int(input("Bir Sayı Giriniz:"))
+print("Girilen sayının karesi:",sayi*sayi)
+print("Girilen sayının küpü:",sayi*sayi*sayi)
